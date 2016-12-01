416397d431ec83b7f0ffb70cc3e3a5c968ebd36c
Vladimir Velkov <vladvv2m@gmail.com>
2016-12-01 17:07:26 +0200

    User can delete his own ads

--- Ads/Controllers/UserController.cs
+++ Ads/Controllers/UserController.cs
@@ -5,23 +5,24 @@
     using System.Linq;
     using Models;
     using ViewModels;
+    using System.Collections.Generic;
 
     public class UserController : BaseController
     {
         [HttpGet]
         [Authorize]
-        public ActionResult Create()
+        public ActionResult CreateAd()
         {
             return View();
         }
 
         [HttpPost]
         [Authorize]
-        public ActionResult Create(AdViewModel adViewModel)
+        public ActionResult CreateAd(AdViewModel adViewModel)
         {
             if (!ModelState.IsValid)
             {
-                return RedirectToAction("Index");
+                return RedirectToAction("ListAds", "User");
             }
 
             var user = this.Context.Users.FirstOrDefault(u => u.Email == HttpContext.User.Identity.Name);
@@ -37,7 +38,35 @@
             this.Context.Ads.Add(newAdad);
             this.Context.SaveChanges();
 
-            return RedirectToAction("Index");
+            return RedirectToAction("ListAds", "User");
+        }
+
+        public ActionResult ListAds()
+        {
+            var user = this.Context.Users.FirstOrDefault(u => u.Email == HttpContext.User.Identity.Name);
+
+            ICollection<AdViewModel> userAds = Context.Ads.Select(a => new AdViewModel()
+            {
+                Id = a.Id,
+                Title = a.Title,
+                Content = a.Content,
+                CreatedOn = a.CreatedOn,
+                User = a.User
+            })
+            .Where(a => a.User.UserName == user.UserName)
+            .ToList();
+            
+            return View(userAds);
+        }
+
+        public ActionResult DeleteAd(int? id)
+        {
+            var ad = Context.Ads.Find(id);
+
+            Context.Ads.Remove(ad);
+            Context.SaveChanges();
+
+            return RedirectToAction("ListAds", "User");
         }
     }
 }
