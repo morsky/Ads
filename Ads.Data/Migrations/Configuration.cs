ae5010f7ecba0aaaf83c567b93207d57bea72fe0
Vladimir Velkov <vladvv2m@gmail.com>
2016-12-05 15:16:10 +0200

    Add Admin user on DB create

---
+++ Ads.Data/Migrations/Configuration.cs
@@ -0,0 +1,53 @@
+namespace Ads.Data.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Ads.Data.AdsDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Ads.Data.AdsDbContext";
+        }
+
+        protected override void Seed(Ads.Data.AdsDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "Admin" };
+
+                manager.Create(role);
+            }
+
+            if (!context.Users.Any(u => u.Email == "admin@admin.com"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "admin@admin.com", Email = "admin@admin.com" };
+
+                manager.Create(user, "QW2#@daq");
+                manager.AddToRole(user.Id, "Admin");
+            }
+        }
+    }
+}
