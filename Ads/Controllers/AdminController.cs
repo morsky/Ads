34eccd0f7afa704718e6f998e15698532bc1378d
Vladimir Velkov <vladvv2m@gmail.com>
2016-12-05 17:28:45 +0200

    Admin user can list all users

---
+++ Ads/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿namespace Ads.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+    using ViewModels;
+    public class AdminController : BaseController
+    {
+        [HttpGet]
+        public ActionResult ListUsers()
+        {
+            //var admin = this.Context.Users.Where(u => u.Email != "admin@admin.com").ToList();
+            var admin = this.Context.Users.FirstOrDefault(a => a.Email == "admin@admin.com");
+
+            ICollection<ApplicationUserViewModel> users = Context.Users.Select(u => new ApplicationUserViewModel()
+            {
+                UserName = u.UserName,
+                Email = u.Email,
+            })
+            .Where(u => u.Email != admin.Email)          
+            .ToList();            
+
+            return View(users);
+        }
+    }
+}
---
+++ Ads/ViewModels/ApplicationUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Ads.ViewModels
+{
+    using Models;
+
+    public class ApplicationUserViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
