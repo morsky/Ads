eb8a895f39e928a147e4c34cbaf85db9f15b0809
Vladimir Velkov <vladvv2m@gmail.com>
2016-12-08 15:36:39 +0200

    Admin can delete user. The user's ads and image files are deleted, too

--- Ads/Controllers/AdminController.cs
+++ Ads/Controllers/AdminController.cs
@@ -9,18 +9,52 @@
         [HttpGet]
         public ActionResult ListUsers()
         {
-            //var admin = this.Context.Users.Where(u => u.Email != "admin@admin.com").ToList();
             var admin = this.Context.Users.FirstOrDefault(a => a.Email == "admin@admin.com");
 
             ICollection<ApplicationUserViewModel> users = Context.Users.Select(u => new ApplicationUserViewModel()
             {
+                Id = u.Id,
                 UserName = u.UserName,
-                Email = u.Email,
+                Email = u.Email
             })
             .Where(u => u.Email != admin.Email)
             .ToList();
 
             return View(users);
+        }
+
+        [Authorize]
+        public ActionResult DeleteUser(string id)
+        {            
+            var currentUser = Context.Users.Find(id);
+            int[] adIds = new int[currentUser.Ads.Count];            
+            int count = 0;
+
+            foreach (var item in currentUser.Ads)
+            {
+                var addNumber = item.Id;                
+                adIds[count] = addNumber;
+
+                count++;                
+            }
+
+            for (int i = 0; i < adIds.Length; i++)
+            {
+                var ad = Context.Ads.Find(adIds[i]);
+                var fullPath = Server.MapPath("~/Images/AdsImages/" + ad.FileName);                                              
+
+                if (System.IO.File.Exists(fullPath))
+                {
+                    System.IO.File.Delete(fullPath);
+                }
+
+                Context.Ads.Remove(ad);
+            }
+
+            Context.Users.Remove(currentUser);
+            Context.SaveChanges();
+
+            return RedirectToAction("ListUsers", "Admin");
         }
     }
 }
--- Ads/ViewModels/ApplicationUserViewModel.cs
+++ Ads/ViewModels/ApplicationUserViewModel.cs
@@ -1,11 +1,13 @@
 ﻿namespace Ads.ViewModels
 {
     using Models;
-
+    using System;
     public class ApplicationUserViewModel
     {
+        public string Id { get; set; }
+
         public string UserName { get; set; }
 
-        public string Email { get; set; }
+        public string Email { get; set; }        
     }
 }
