6fbb40bd7c693707a9af142e3178679fdf39f10e
Vladimir Velkov <vladvv2m@gmail.com>
2016-12-08 17:53:42 +0200

    Admin alwmost can edit user role

--- Ads/Controllers/AdminController.cs
+++ Ads/Controllers/AdminController.cs
@@ -1,5 +1,8 @@
 ﻿namespace Ads.Controllers
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System.Collections.Generic;
     using System.Linq;
     using System.Web.Mvc;
@@ -23,25 +26,57 @@
             return View(users);
         }
 
+        [HttpGet]
+        [Authorize]
+        public ActionResult EditUserRole(string id)
+        {
+            var selectedUser = Context.Users.Find(id);
+
+            ViewBag.User = selectedUser.UserName.ToString();
+
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(Context));
+            var userRole = UserManager.GetRoles(selectedUser.Id);
+            
+            ViewBag.UserRoles = userRole;
+
+            var roles = Context.Roles.OrderBy(x => x.Name);
+            List<string> userRoles = new List<string>();
+
+            foreach (var role in roles)
+            {
+                userRoles.Add(role.Name);
+            }            
+
+            return View(userRoles);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult EditUserRole(List<string> role)
+        {
+
+            return View();
+        }
+
         [Authorize]
         public ActionResult DeleteUser(string id)
-        {            
-            var currentUser = Context.Users.Find(id);
-            int[] adIds = new int[currentUser.Ads.Count];            
+        {
+            var selectedUser = Context.Users.Find(id);
+            int[] adIds = new int[selectedUser.Ads.Count];
             int count = 0;
 
-            foreach (var item in currentUser.Ads)
+            foreach (var item in selectedUser.Ads)
             {
-                var addNumber = item.Id;                
+                var addNumber = item.Id;
                 adIds[count] = addNumber;
 
-                count++;                
+                count++;
             }
 
             for (int i = 0; i < adIds.Length; i++)
             {
                 var ad = Context.Ads.Find(adIds[i]);
-                var fullPath = Server.MapPath("~/Images/AdsImages/" + ad.FileName);                                              
+                var fullPath = Server.MapPath("~/Images/AdsImages/" + ad.FileName);
 
                 if (System.IO.File.Exists(fullPath))
                 {
@@ -51,7 +86,7 @@
                 Context.Ads.Remove(ad);
             }
 
-            Context.Users.Remove(currentUser);
+            Context.Users.Remove(selectedUser);
             Context.SaveChanges();
 
             return RedirectToAction("ListUsers", "Admin");
